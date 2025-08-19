4dcd4f9746fb383f4b3e7a011ed4de09430248d8
tiago-xavier-braga <braga.tiagoxavier@gmail.com>
2025-08-18 23:07:04 -0300

    feat-code: Created code for sound car

---
+++ Assets/Code/Scripts/Car/CarController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace XaviGames.Car
+{
+    public class CarController : MonoBehaviour
+    {
+        [field: SerializeField]
+        public bool IsMovementEnabled { get; private set; } = true;
+
+
+    }
+}
---
+++ Assets/Code/Scripts/Car/CarSoundController.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using XaviEssencials.Runtime;
+
+namespace XaviGames.Car
+{
+    public class CarSoundController : MonoBehaviour
+    {
+        [SerializeField]
+        private CarMovementController _carMovementController;
+
+        [SerializeField]
+        private CarController _carController;
+
+        [Header("Audio Sources References")]
+        [SerializeField]
+        private AudioSource _audioSource;
+
+        [Header("Clips References")]
+        [SerializeField]
+        private AudioClip _audioClip;
+
+        [SerializeField]
+        [Range(0, 1f)]
+        private float _volume = 0.5f;
+
+        [SerializeField]
+        private float _minMovingPitch = 0.8f;
+
+        [SerializeField]
+        private float _maxMovingPitch = 1.2f;
+
+        private void Update()
+        {
+            if (!_carController.IsMovementEnabled)
+            {
+                if (_audioSource.isPlaying)
+                {
+                    _audioSource.Stop();
+                }
+
+                return;
+            }
+
+            if (!_audioSource.isPlaying)
+            {
+                _audioSource.clip = _audioClip;
+                _audioSource.volume = _volume;
+                _audioSource.Play();
+            }
+            float pitch = Mathf.Lerp(_minMovingPitch, _maxMovingPitch,
+                                     Mathf.InverseLerp(0f, 100f, _carMovementController.KmPerHour));
+            _audioSource.pitch = pitch;
+        }
+    }
+}
