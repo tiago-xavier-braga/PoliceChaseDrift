33660ac78d0c1a9a37dee5fd799abb65ccc1f973
tiago-xavier-braga <braga.tiagoxavier@gmail.com>
2025-08-25 22:43:28 -0300

    feat-code: Created object variable for float

---
+++ Assets/Code/Scripts/ObjectVariable/FloatVariable.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Events;
+using XaviEssencials.Runtime;
+
+namespace XaviGames.ObjectVariable
+{
+    [CreateAssetMenu(fileName = "FloatVariable", menuName = "Xavi Games/ObjectVariable/FloatVariable", order = 1)]
+    public class FloatVariable : ScriptableObject
+    {
+        [SerializeField]
+        private float _value;
+
+        [field: SerializeField]
+        [field: ReadOnly]
+        public float Value { get; private set; }
+
+        public UnityAction<float> OnValueChanged;
+
+        private void OnEnable()
+        {
+            Value = _value;
+        }
+
+        public void SetValue(float value)
+        {
+            Value = value;
+            OnValueChanged?.Invoke(Value);
+        }
+    }
+}
