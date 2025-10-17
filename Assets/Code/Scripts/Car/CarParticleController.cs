e377b061edd8154689f5d61706b26fa333f16acc
tiago-xavier-braga <braga.tiagoxavier@gmail.com>
2025-10-16 22:33:52 -0300

    feat-code: Created code for enable the particles

---
+++ Assets/Code/Scripts/Car/CarParticleController.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics.Tracing;
+using UnityEngine;
+using XaviGames.Player;
+
+namespace XaviGames.Car
+{
+    public class CarParticleController : MonoBehaviour
+    {
+        [SerializeField]
+        private ParticleSystem _particleSystem;
+
+        [SerializeField]
+        private PlayerHealth _playerHealth;
+
+        [SerializeField]
+        private float _healthThreshold;
+
+        private void OnEnable()
+        {
+            _playerHealth.OnHealthChanged += OnHealthChanged;
+        }
+
+        private void OnDisable()
+        {
+            _playerHealth.OnHealthChanged -= OnHealthChanged;
+        }
+
+        private void OnHealthChanged(float health)
+        {
+            if (health < _healthThreshold && !_particleSystem.isPlaying)
+            {
+                _particleSystem.Play();
+            }
+            else if (health >= _healthThreshold && _particleSystem.isPlaying)
+            {
+                _particleSystem.Stop();
+            }
+        }
+    }
+}
